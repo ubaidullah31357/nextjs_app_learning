794261aac2ff4804f545c57c0f043e8e2f0364d9
ubaid-ullah <ubaidullah31357@gmail.com>
2026-07-30 17:01:32 +0500

    global css(for all pages)  and css modules(for specific page) lesson was added

--- app/(users)/blog/page.tsx
+++ app/(users)/blog/page.tsx
@@ -1,7 +1,8 @@
 import { JSX } from "react";
+import style from "./blog.module.css";
 
 function Blog(): JSX.Element {
-  return <h1>User Blog Page</h1>;
+  return <h1 className={style.heading}>User Blog Page</h1>;
 }
 
 export default Blog;
