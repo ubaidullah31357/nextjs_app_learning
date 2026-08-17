2df0299a42cc6537a8c30aa7a054cb33484d7056
ubaid-ullah <ubaidullah31357@gmail.com>
2026-08-17 07:58:49 +0500

    useActionState Hook added to the contact form. This hook is used to update state based on the result of the form action. It gives us Pending state, state (return value or error of the form action.

--- app/(users)/contact/contact.action.tsx
+++ app/(users)/contact/contact.action.tsx
@@ -2,16 +2,30 @@
 
 import { db } from "@/config/db";
 
-export const contactAction = async (formData: any) => {
+export const contactAction = async (previousState: any, formData: any) => {
   //   const name = formData.get("name");
   //   const email = formData.get("email");
   //   const message = formData.get("message");
 
-  const { name, email, message } = Object.fromEntries(formData.entries());
-  console.log(name, email, message);
+  try {
+    const { name, email, message } = Object.fromEntries(formData.entries());
+    console.log(name, email, message);
 
-  await db.execute(
-    `insert into contact_form (name, email, message) values (?, ?, ?)`,
-    [name, email, message],
-  );
+    // await db.execute(
+    //   `insert into contact_form (name, email, message) values (?, ?, ?)`,
+    //   [name, email, message],
+    // );
+
+    
+    return {
+      success: true,
+      message: "Form has been submitted Successfully.",
+    };
+  } catch (error: any) {
+    console.log("Some Error has occured ⚠");
+    return {
+      success: false,
+      message: "Some Error has occured while submitting the Form.",
+    };
+  }
 };
--- app/(users)/contact/page.tsx
+++ app/(users)/contact/page.tsx
@@ -1,58 +1,72 @@
 "use client";
 
-import { JSX } from "react";
-// import { contactAction } from "./contact.action";
+import { JSX, useActionState } from "react";
+import { contactAction } from "./contact.action";
 
-const contactAction = (formData: any) => {
-  const { name, email, message } = Object.fromEntries(formData.entries());
-  console.log(name, email, message);
-};
+// const contactAction = (formData: any) => {
+//   const { name, email, message } = Object.fromEntries(formData.entries());
+//   console.log(name, email, message);
+// };
 
 function Contact(): JSX.Element {
-  return (
-    <form
-      action={contactAction}
-      className="bg-gray-800 rounded p-5 pl-36 flex flex-col items-start justify-start gap-5 text-white text-sm font-bold"
-    >
-      <label htmlFor="name" className="flex flex-col gap-1">
-        Full Name
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Enter your full name"
-          className="bg-gray-500 text-white rounded-sm p-3 h-9 w-130"
-        />
-      </label>
-      <label htmlFor="email" className="flex flex-col gap-1">
-        Email Address
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter your email address"
-          className="bg-gray-500 text-white rounded-sm p-3 h-9 w-130"
-        />
-      </label>
-      <label htmlFor="message" className="flex flex-col gap-1">
-        Message
-        <textarea
-          name="message"
-          id="message"
-          cols={60}
-          rows={8}
-          placeholder="Enter your message"
-          className="bg-gray-500 text-white rounded-sm p-3"
-        ></textarea>
-      </label>
+  // const [state, formAction, isPending] = useActionState(fn, initialState, permalink?);
+  const [state, formAction, isPending] = useActionState(contactAction, null);
 
-      <button
-        type="submit"
-        className="bg-pink-500 w-130 p-2 rounded cursor-pointer hover:bg-pink-600"
+  return (
+    <>
+      <form
+        action={formAction}
+        className="bg-gray-800 rounded p-5 pl-36 flex flex-col items-start justify-start gap-5 text-white text-sm font-bold"
       >
-        Send Message
-      </button>
-    </form>
+        <label htmlFor="name" className="flex flex-col gap-1">
+          Full Name
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Enter your full name"
+            className="bg-gray-500 text-white rounded-sm p-3 h-9 w-130"
+          />
+        </label>
+        <label htmlFor="email" className="flex flex-col gap-1">
+          Email Address
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email address"
+            className="bg-gray-500 text-white rounded-sm p-3 h-9 w-130"
+          />
+        </label>
+        <label htmlFor="message" className="flex flex-col gap-1">
+          Message
+          <textarea
+            name="message"
+            id="message"
+            cols={60}
+            rows={8}
+            placeholder="Enter your message"
+            className="bg-gray-500 text-white rounded-sm p-3"
+          ></textarea>
+        </label>
+
+        <button
+          type="submit"
+          className="bg-pink-500 w-130 p-2 rounded cursor-pointer hover:bg-pink-600"
+          disabled={isPending}
+        >
+          {isPending ? "Loading..." : "Send Message"}
+        </button>
+      </form>
+
+      {state && (
+        <section
+          className={`p-5 mb-3 ${state?.success ? "bg-green-500" : "bg-red-500"}`}
+        >
+          <p>{state?.message}</p>
+        </section>
+      )}
+    </>
   );
 }
 export default Contact;
